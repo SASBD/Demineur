47a7e89e40c3a582e144df0719cc6bea0bd47a94
sofiane.bastenier <sofiane.bastenier@etu.u-paris.fr>
2024-11-18 15:29:30 +0100

    pushed

--- Demineur/Main.c
+++ Demineur/Main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
-enum {DEFAULT_SIZE = 11};
-enum {VIDE,MINE,DEMINE};
+#include <stdlib.h>
+enum { DEFAULT_SIZE = 11 };
+typedef enum { VIDE, MINE, DEMINE } Etat;
 
 
-void initialisation(int tab[][DEFAULT_SIZE]) { // remplacer plus tard avec malloc (enregistre la taille max du tab puis on envoie avec un pointeur et on reutilise la taille n)
+void initialisation(Etat tab[][DEFAULT_SIZE]) { // remplacer plus tard avec malloc (enregistre la taille max du tab puis on envoie avec un pointeur et on reutilise la taille n)
 	for (unsigned int i = 0; i < DEFAULT_SIZE; i++) {
 		for (unsigned int j = 0; j < DEFAULT_SIZE; j++) {
 			tab[i][j] = VIDE;
@@ -12,7 +13,7 @@ void initialisation(int tab[][DEFAULT_SIZE]) { // remplacer plus tard avec mallo
 	}
 }
 
-void affichePartie(unsigned int tab[DEFAULT_SIZE][DEFAULT_SIZE]) {
+void affichePartie(Etat tab[DEFAULT_SIZE][DEFAULT_SIZE]) {
 	printf("  ");
 	for (unsigned int i = 0; i < DEFAULT_SIZE; i++) {
 		printf(" %u", i);
@@ -28,6 +29,36 @@ void affichePartie(unsigned int tab[DEFAULT_SIZE][DEFAULT_SIZE]) {
 		}
 		printf("%c", '\n');
 	}
+}
+/*
+unsigned int s = (unsigned int) time (NULL);
+printf("%u\n, s);
+srand(s);
+*/
+void placerBombes(unsigned int m) {
+	unsigned int j = 0;
+	for (int i = 0; m > 0, i++) // je remplis par ligne pas par colonne
+	{
+		tab[i][j] == MINE;
+		if (i == DEFAULT_SIZE - 1) {
+			j++;
+			i = 0;
+		}
+		m--;
+	}
+	if (m != DEFAULT_SIZE * DEFAULT_SIZE) {
+		for (int i = DEFAULT_SIZE * DEFAULT_SIZE - 1; i > 0; i--)
+		{
+			unsigned int j = rand() % (i + 1);
+			Etat temp = tab[i % DEFAULT_SIZE][i / DEFAULT_SIZE];
+			tab[i % DEFAULT_SIZE][i / DEFAULT_SIZE] = tab[j % DEFAULT_SIZE][j / DEFAULT_SIZE];
+			tab[j % DEFAULT_SIZE][j / DEFAULT_SIZE] = temps;
+		}
+	}
+}
+
+void statutPartie() {
+
 }
 
 int main(void) {
